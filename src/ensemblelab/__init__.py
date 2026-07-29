4716db2d90cb566c81753afe2923b4d5f8cc81ed
Dhruv Avirneni <dhruv.avirneni@gmail.com>
2026-07-28 23:40:30 -0500

    updated

--- src/ensemblelab/__init__.py
+++ src/ensemblelab/__init__.py
@@ -1,6 +1,6 @@
 """Tools for reproducible molecular conformational-ensemble analysis."""
 
 from .generators import Conformer, Ensemble, generate
-from .optimize import optimize
+from .optimize import hierarchical_optimize, optimize
 
-__all__ = ["Conformer", "Ensemble", "generate", "optimize"]
+__all__ = ["Conformer", "Ensemble", "generate", "hierarchical_optimize", "optimize"]
