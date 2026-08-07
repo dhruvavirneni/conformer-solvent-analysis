1ca5abfa7fc36923b651a5c030f0cfb8ec873930
Dhruv <dhruv.avirneni@gmail.com>
2026-08-07 00:52:04 -0500

    adjusted processing to use helper functions for metadata handling ease across filter modules

--- src/ensemblelab/filters/energy.py
+++ src/ensemblelab/filters/energy.py
@@ -1,8 +1,6 @@
 from ensemblelab.generators import Ensemble
-from abc import ABC, abstractmethod
 from ensemblelab.filters.base import BaseFilter
 from dataclasses import dataclass
-from copy import deepcopy
 
 @dataclass(frozen=True, slots=True)
 class EnergyFilter(BaseFilter):
@@ -14,26 +12,16 @@ class EnergyFilter(BaseFilter):
         self.top_n = top_n
   
     def apply(self, ensemble: Ensemble) -> Ensemble:
+        self._validate_ensemble(ensemble)
         if self.window is not None:
-            metadata = deepcopy(ensemble.metadata)
             ensemble_energies = [conformer.energy for conformer in ensemble.conformers if conformer.energy is not None]
             min_energy = min(ensemble_energies)
             filtered_conformers = [conformer for conformer in ensemble.conformers if conformer.energy is not None and conformer.energy - min_energy <= self.window]
-            return Ensemble(
-                smiles=ensemble.smiles,
-                molecule=ensemble.molecule,
-                conformers=tuple(filtered_conformers),
-                metadata=metadata
+            return self._build_filtered_ensemble(ensemble, filtered_conformers, {"window": self.window}
 )
         elif self.top_n is not None:
-            metadata = deepcopy(ensemble.metadata)
             ensemble_energies = [conformer.energy for conformer in ensemble.conformers if conformer.energy is not None]
             min_energy = min(ensemble_energies)
-            sorted_conformers = sorted(ensemble.conformers, key=lambda c: c.energy - min_energy)
+            sorted_conformers = sorted(ensemble.conformers, key=lambda c: c.energy)
             filtered_conformers = sorted_conformers[:self.top_n]
-            return Ensemble(
-                smiles=ensemble.smiles,
-                molecule=ensemble.molecule,
-                conformers=tuple(filtered_conformers),
-                metadata=metadata
-)
+            return self._build_filtered_ensemble(ensemble, filtered_conformers, {"top_n": self.top_n})
